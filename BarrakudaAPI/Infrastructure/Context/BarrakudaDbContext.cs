7355e4dda2ad0ec787fdb279cf2b68d54babe510
Szymon Sowierszenko <s.sowierszenko@student.po.edu.pl>
2022-03-12 17:10:48 +0100

    Added DbContext class

---
+++ BarrakudaAPI/Infrastructure/Context/BarrakudaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class BarrakudaDbContext : DbContext
+    {
+        public BarrakudaDbContext(DbContextOptions<BarrakudaDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
