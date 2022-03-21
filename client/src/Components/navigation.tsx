e3ed5ba575ac9917f10460a9ec8fb03b4af76ce5
Zuzanna Myśliwa <zuza.mysliwa@student.po.edu.pl>
2022-03-21 18:35:08 +0100

    Added main page template

---
+++ client/src/Components/navigation.tsx
@@ -0,0 +1,46 @@
+﻿import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+
+
+export default function Navigation() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    return (
+        <Box sx={{ pb: 7 }} ref={ref}>
+            <CssBaseline />
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="Kontakt"/>
+                    <BottomNavigationAction label="Centrum Pomocy" />
+                    <BottomNavigationAction label="O nas" />
+                </BottomNavigation>
+            </Paper>
+        </Box>
+    );
+}
+
+interface MessageExample {
+    primary: string;
+    secondary: string;
+    person: string;
+}
+
